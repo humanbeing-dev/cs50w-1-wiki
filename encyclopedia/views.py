c5d48d3562984206e9ca9b89dbfd8ee5b6bd8a7a
humanbeing-dev <maciej.sitowski@gmail.com>
2021-01-08 13:35:56 +0100

    Resolve case differences

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -9,32 +9,19 @@ def index(request):
 
 
 def wiki(request, title):
-    # Get all entries
+    """ Function that checks if given title is within entries list """
     entries = util.list_entries()
+    content = util.get_entry(title)
 
-    try:
-        # Try to find an entry casefolded in entries casefolded
-        index = [entry.casefold() for entry in entries].index(title.casefold())
-
-
-
-        # Get casefolded entry
-        content = util.get_entry(entries[index])
+    if content:
         return render(request, "encyclopedia/entry.html", {"content": content, "title": title})
+    else:
+        try:
+            # Try to find an entry casefolded in entries casefolded
+            index = [entry.casefold() for entry in entries].index(title.casefold())
+            # Redirect with proper CASE
+            return redirect(to="wiki", title=entries[index])
 
-    except ValueError:
-        message = "404 - Such entry does not exist in encyclopedia"
-        return render(request, "encyclopedia/404.html", {"content": message, "title": "Not found"})
-
-
-
-class Entry:
-    def __init__(self, title):
-        self.title = title
-
-    def is_casefolded_in_entries(self, entries):
-        casefolded =  [entry.casefold() for entry in entries]
-    
-
-    def is_exact_in_entries(self, entries):
-        pass
+        except ValueError:
+            message = "Such entry does not yet exist in encyclopedia"
+            return render(request, "encyclopedia/404.html", {"content": message, "title": "Not found"})
